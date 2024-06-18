4b1b1c81c0b349a6c8666c9716df91e4f2696ebd
alvaro luis castaño <alvarocastano_@outlook.com>
2024-06-18 22:34:07 +0200

    utilizando ciclos y condicionales

--- js/main.js
+++ js/main.js
@@ -1,21 +1,40 @@
-function holaMundo(){
-    console.log("hola mundo")
-}
-
-holaMundo()
-
-const nombre = prompt("Ingrese su nombre");
-const pagoXhora = parseFloat(prompt("Ingrese su pago x horas"));
-const horasTrabajadas = parseInt(prompt("Ingrese cantidad de horas trabajadas x dia"));
-const diasTrabajados = parseInt(prompt("Ingrese cantidad de dias trabajados"));
 
 
+const name = prompt("Ingrese su nombre");
 
-const sueldoTotal = (pagoXhora, horasTrabajadas,diasTrabajados) => {
-    let sueldo = ((pagoXhora * horasTrabajadas) * diasTrabajados)
-    console.log("estoy aqui")
-    console.log("su sueldo es:" + sueldo)
-    return sueldo;
-}
+let payPerHours;
+do {
+    payPerHours = parseFloat(prompt("Ingrese su pago x horas"));
+    if (payPerHours > (3000000 / 30 / 8)) {
+        alert("Su pago por hora es muy elevado, tenga en cuenta que lo máximo que puede ganar por mes es 3000000.");
+    }
+} while (payPerHours > (3000000 / 30 / 8));
 
-alert( nombre + "su sueldo es:" + sueldo);
+let hoursWorked;
+do {
+    hoursWorked = parseInt(prompt("Ingrese cantidad de horas trabajadas x día"));
+    if (hoursWorked > 8) {
+        alert("Ya registró el máximo de horas por día. Por favor, ingrese nuevamente.");
+    }
+} while (hoursWorked > 8);
+
+
+let daysWorked;
+do {
+    daysWorked = parseInt(parseInt(prompt("Ingrese cantidad de días trabajados")));
+    if (hoursWorked > 8) {
+        alert("La cantidad maxima de dias trabajaod son 30.");
+    }
+} while (daysWorked > 30);
+
+const sueldoTotal = (payPerHours, hoursWorked, daysWorked) => {
+    return (payPerHours * hoursWorked) * daysWorked;
+};
+
+const sueldo = sueldoTotal(payPerHours, hoursWorked, daysWorked);
+
+alert(name + ", su sueldo es: " + sueldo);
+
+
+
+
